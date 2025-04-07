b96a0a97ec9a218011e5b18a15011d156c141d3d
Alba Segura <2023_alba.segura@gmail.com>
2025-04-07 15:16:31 +0200

    request to the api

--- src/server.py
+++ src/server.py
@@ -8,15 +8,14 @@ dict_torurnaments = {}
 async def handle_client(reader, writer):
     addr = writer.get_extra_info('peername')
     print(f"Connection from {addr}")
-
-    data = await reader.read(100)  # Read up to 100 bytes
+    data = await reader.read(100)
     print(f"Received: {data.decode()} from {addr}")
-
     data = data.decode().strip()
     data = data.split(",")
+    data = data.replace("'", "") 
+    data = data.replace("[", "") 
+    data = data.replace("]", "") 
     print(f"Data: {data}")
-    
-    # Respond to the client
     writer.write(b"Data received!\n")
     await writer.drain()
     
@@ -31,33 +30,25 @@ async def periodic_get_request():
                 async with session.get(url) as response:
                     print(f"GET {url} - Status: {response.status}")
                     if response.status == 200:
-                        # Parse the response as JSON
                         data = await response.json()
-                        # Debugging: Print the raw data
-                        #print(f"Raw response data: {data}")
-                        # Extract and print `id` and `nom` fields
                         for item in data:
-                            #print(f"Raw item data: {item}")  # Debugging
-                            id_tournament = item.get('id_torneig')  # Ensure id_tournament is a string
+                            id_tournament = item.get('id_torneig')  
                             nom = item.get('nom')
                             if id_tournament not in dict_torurnaments:
-                                dict_torurnaments[id_tournament] = {}
+                                dict_torurnaments[''+id_tournament] = {}
                             print(f"id: {id_tournament}, nom: {nom}")
                     else:
                         print(f"Failed to fetch data. Status: {response.status}")
             except Exception as e:
                 print(f"Error during GET request: {e}")
         await asyncio.sleep(2)
-        i += 1  # Wait for 2 seconds before the next request
+        i += 1 
     print("Finished periodic GET requests.")
     for id_tournament, data in dict_torurnaments.items():
         print(f"ID: {id_tournament}, Data: {data}")
 
 async def main():
-    # Start the periodic GET request task
     asyncio.create_task(periodic_get_request())
-    
-    # Start the server
     server = await asyncio.start_server(handle_client, HOST, PORT)
     addr = server.sockets[0].getsockname()
     print(f"Server running on {addr}")
