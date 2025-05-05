6bc752c0d49e71ed48c21012c40c99c2b7768405
Alba Segura <m1ss.sunrse@gmail.com>
2025-05-05 20:53:05 +0200

    fix: update notification codes for tournament status in notify_tournament_players function

--- src/server.py
+++ src/server.py
@@ -225,9 +225,9 @@ async def notify_tournament_players(tournament,code):
 
             num = await player.send_message(notification)
             if tournament.status == "started":
-                await player.send_message(f"{code}.{tournament.id_torneig}.pairing\n")
+                await player.send_message(f"2.{tournament.id_torneig}.pairing\n")
             if tournament.status == "finished":
-                await player.send_message(f"{code}.{tournament.id_torneig}.end\n")
+                await player.send_message(f"3.{tournament.id_torneig}.end\n")
             if num == 1:
                 print(f"Failed to send message to player {player.id_jugador}. Removing from tournament.")
                 await remove_disconnected_player(player, tournament)
