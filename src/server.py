0a777ecd040f2534b996143e64884766c2344e14
Alba Segura <m1ss.sunrse@gmail.com>
2025-05-07 20:42:43 +0200

    fix: notify and disconnect players at the end of the tournament in make_parings function

--- src/server.py
+++ src/server.py
@@ -200,6 +200,18 @@ async def make_parings(tournament):
             else:
                 await notify_tournament_players(tournament, 3)
                 tournament.status = "finished"
+                for player in players:  
+                    if player.id_torneig == tournament.id_torneig:
+                        try:
+                            print(f"Notified player {player.id_jugador} about tournament end.")
+                            player.writer.close()
+                        except (ConnectionResetError, BrokenPipeError):
+                            print(f"Failed to notify player {player.id_jugador}.")
+                        tournament.players.remove(player)
+                        players.remove(player)
+                        print(f"Player {player.id_jugador} removed from tournament {tournament.id_torneig}.")
+
+                print(f"All players disconnected from tournament {tournament.id_torneig}.")
             
 
                 
